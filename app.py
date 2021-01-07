b610fcd48f8a7b217c5025b4da236f0303b621ab
Sajeel Hassan <hsajeel.dev@gmail.com>
2021-01-07 16:22:38 +0500

    routes added

--- app.py
+++ app.py
@@ -13,48 +13,76 @@ def incInventory():
     return jsonify(myinventory)
 
 
-@app.route('/')
-@app.route('/login')
+@app.route('/incAddproduct', methods=['POST'])
+def incAddproduct():
+    response = request.get_json(silent=True)
+    obj = DBFns('localhost', 'root', 's@ajeel', 'wms')
+    message = 'Database/Server Error!'
+    try:
+        if obj.isSkuExists(response['sku']) != True:
+            if obj.addProduct(response['title'], response['sku'], response['cost'],
+                              response['price'], response['stock'], response['low']):
+                message = 'true'
+        elif obj.isSkuExists(response['sku']) == True:
+            message = 'SKU Already Exist!'
+    except Exception as e:
+        print(str(e))
+        message = 'Database/Server Error!'
+    finally:
+        return message
+
+
+@app.route('/incGetproduct', methods=['POST'])
+def incGetproduct():
+    data = request.get_json(silent=True)
+    print(data['prod_id'])
+    obj = DBFns('localhost', 'root', 's@ajeel', 'wms')
+    reqProd = obj.getProduct(data['prod_id'])
+    return jsonify(reqProd)
+
+
+@ app.route('/')
+@ app.route('/login')
 def form():
     return render_template('./admin/login.html')
 
 
-@app.route('/dashboard')
+@ app.route('/dashboard')
 def home():
     return render_template('./admin/dashboard.html')
 
 
-@app.route('/inventory')
+@ app.route('/inventory')
 def showInventory():
     return render_template('./admin/inventory.html')
 
 
-@app.route('/receipts')
+@ app.route('/receipts')
 def showReceipts():
     return render_template('./admin/receipts.html')
 
 
-@app.route('/customers')
+@ app.route('/customers')
 def showCustomers():
     return render_template('./admin/customers.html')
 
 
-@app.route('/reports')
+@ app.route('/reports')
 def showReports():
     return render_template('./admin/reports.html')
 
 
-@app.route('/employees')
+@ app.route('/employees')
 def showEmployees():
     return render_template('./admin/employees.html')
 
 
-@app.route('/support')
+@ app.route('/support')
 def menuSupport():
     return render_template('./admin/support.html')
 
 
-@app.route('/settings')
+@ app.route('/settings')
 def adminSettings():
     return render_template('./admin/settings.html')
 
